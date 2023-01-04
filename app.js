e911257d0ca0109424a7b37d1b1571cfb8626c3a
maria-ys <ys.mariayounas@gmail.com>
2023-01-04 18:40:20 +0500

    created Task Schema

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
-const express = require('express');
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
 const app = express();
-const tasksRoute = require('./routes/tasksRoute');
+const tasksRoute = require("./routes/tasksRoute");
 
 // listening
-const port = 3000;
-app.listen(port, ()=> console.log("listenting requests on port "+ port));
 
-//middleware 
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(process.env.PORT, () => console.log("listenting requests on port " + process.env.PORT));
+  })
+  .catch((err) => console.log(err));
+
+
+//middleware
 app.use(express.json());
-app.use('/api/v1/tasks', tasksRoute)
+app.use("/api/v1/tasks", tasksRoute);
 
-//routes 
-app.get('/hello',(req,res)=>{
-    res.send("Task Manager App");
-})
+//routes
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
 
 // app.get('/api/v1/tasks') - get all the tasks
 // app.post('/api/v1/tasks') - create a new task
 // app.get('/api/v1/tasks/:id') - get single task
 // app. patch('/api/v1/tasks/:id') - update task
 // app.delete('/api/v1/tasks/:id') - delete task
-
