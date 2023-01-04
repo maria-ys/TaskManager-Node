6df54c9d1a3c2b9da9f00f0c49e05f75b7d5047d
maria-ys <ys.mariayounas@gmail.com>
2023-01-04 22:31:33 +0500

    port setup

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use(notFound);
 app.use(errorHandlerMiddleware);
 
 // listening & connected to DB
+const port = process.env.PORT || 3000;
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -23,6 +24,6 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    app.listen(process.env.PORT, () => console.log("listenting requests on port " + process.env.PORT));
+    app.listen(port, () => console.log("listenting requests on port " + port));
   })
   .catch((err) => console.log(err));
